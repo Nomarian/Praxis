1bc512020f14669c3e01f1a11504be74d8285407
khwerz@gmail.com <khwerz@gmail.com>
2019-08-28 20:08:18 +0000

    Lua works now

--- fizzbuzz/lua.lua
+++ fizzbuzz/lua.lua
@@ -1,71 +1,10 @@
-#!/usr/bin/env lua-5.1
+#!/usr/bin/env lua
 
---[[
-function fizzbuzz(to)
- for num=1, to do
-	text = num
-  if num % 3 then
-	print ("fizz")
-  end
-  if num % 5 then
-	print ("buzz")
-  end
- end
+function fizzbuzz(n) -- string
+ if n % 15 == 0	then return "fizzbuzz" end
+ if n % 3 == 0	then return "fizz" end
+ if n % 5 == 0	then return "buzz" end
+ return n
 end
 
-fizzbuzz(10)
-
-  if num % 3 and num % 5 then
-	printf("fizzbuzz")
-  end
-while x >= 100 do
-	if x % 3 and
-	print(fizz)
-	print(buzz)
-	print(x)
-	x=x + 1
-end
-
-
-
-for n = 1, 20 do
-	p=""
-  if (n % 3) == false then
-	p = "fizz"
-  end
-
-  if (n % 5) == false then
-	p = p.."buzz"
-  end
-
-  print(p)
-end
-
---darn this is hard... or mayb its because its late!?
--- really man i got no clue!
-function fizz()
-  io.write("fizz")
-end
-
-function Buzz()
-  io.write("Buzz")
-end
-
-function check(number)
-step = number
-  for multiples3 = number, 100, step do
-    return true
-  end
-end
-
-for start = 1, 100 do
-  if start == check(3) then
-    print(fizz())
-  elseif start == check(5)then
-    io.write(buzz())
-  else
-    print(start)
-  end
-end
-
---]]
+for i=1, 100 do print(fizzbuzz(i)) end
